7feda54e45879b4a9c6a6ea78edc5087ae86dc99
Danny Libin <omegasol11@gmail.com>
2016-03-30 16:11:02 -0500

    working ordinal x-axis

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 require('./styles.css');
 const d3 = require('d3');
 const axios = require('axios');
+const _ = require('lodash');
 
 const w = 600;
 const h = 350;
@@ -19,26 +20,51 @@ function init() {
 function plotData(jsonData) {
 	// Data is an array of arrays, ['quarter date string', gdp number]
 	let dataset = jsonData.data;
-	console.log(dataset);
+	let padding = 24;
 	let xScale = d3.scale.ordinal()
 					.domain(d3.range(dataset.length))
-					.rangeRoundBands([0, w], 0.01);
+					.rangeRoundBands([padding, w - padding], 0.01);
 	let yScale = d3.scale.linear()
 					.domain([0, d3.max(dataset, d => d[1])])
-					.range([0, h]);
+					.range([h, 0]);
+					
+	let xAxisTicks = d3.range(dataset.length).filter(num => num % 10 === 0);
+	let xAxisYears = extractYears(dataset);
+	let xAxis = d3.svg.axis().scale(xScale).orient('bottom').tickValues(xAxisYears);
+	let yAxis = d3.svg.axis().scale(yScale).orient('left').ticks(5);
 	let svg = d3.select('#chart')
 				.append('svg')
-				.attr('width', w)
-				.attr('height', h);
+				.style('padding-left', 30)
+				.attr('width', 650)
+				.attr('height', 400);
+				
+	svg.append('g')
+		.attr('class', 'x axis')
+		.attr('transform', `translate(0, ${h})`)
+		.call(xAxis);
+		
+	svg.append('g')
+		.attr('class', 'y axis')
+		.attr('transform', `translate(${padding}, 0)`)
+		.call(yAxis);
 				
 	svg.selectAll('rect')
 		.data(dataset)
 		.enter()
 		.append('rect')
-		.each(d => console.log(d))
 		.attr('x', (d, i) => xScale(i))
-		.attr('y', d => h - yScale(d[1]))
+		.attr('y', d => yScale(d[1]))
 		.attr('width', xScale.rangeBand())
-		.attr('height', d => yScale(d[1]))
-		.attr('fill', 'rgb(0, 0, 255)');
+		.attr('height', d => h - yScale(d[1]));
+}
+
+function extractYears(dataset) {
+	let numericalYearArray = []
+	dataset.map(set => {
+		let yearOnlyNum = parseInt(set[0].substring(0, 4));
+		if (yearOnlyNum % 5 === 0) numericalYearArray.push(yearOnlyNum);
+	});
+	numericalYearArray = _.sortedUniq(numericalYearArray);
+	console.log(numericalYearArray);
+	return numericalYearArray;
 }
